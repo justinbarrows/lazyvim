a4005d1acc30021627cc8ebaf9e9ff3bc386a4a1
justinbarrows <jbarrows1030@gmail.com>
2024-07-22 20:22:34 -0400

    upload nvim cfg

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add theme
+  { "EdenEast/nightfox.nvim" },
+
+  -- Configure LazyVim to load theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
